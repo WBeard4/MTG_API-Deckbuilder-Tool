c522d4d82b25707cd78e78f7b9047d1fca6a0d34
WBeard4 <wmb6598@gmail.com>
2024-08-06 10:45:25 +0100

    Decklist class created, initial structure laid out

---
+++ decklist.py
@@ -0,0 +1,16 @@
+class Decklist:
+    def __init__(self, decklist, decklist_name):
+        self.decklist = decklist
+        self.decklist_name = decklist_name
+
+    def add_to_decklist():
+        pass
+
+    def remove_from_decklist():
+        pass
+
+    def show_current_decklist():
+        pass
+
+    def save_decklist():
+        pass
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #https://github.com/MagicTheGathering/mtg-sdk-python
+# Currently working towards a text based decklist builder. You will be able to add and remove cards, find out mana costs and colour identities, more features once this is all implemented
+# Decklists will be saved as using just the multiverse_id, which is a unique 6 digit identifier for each card, and the fastest way to find each card. This allows the data saved to be much smaller
 
-# Import classes from API
+# Import classes from API, likely only going to be using Card, grabbing all now for testing
 from mtgsdk import Card
 from mtgsdk import Set
 from mtgsdk import Type
@@ -8,16 +10,75 @@ from mtgsdk import Supertype
 from mtgsdk import Subtype
 from mtgsdk import Changelog
 
-query = input("Please input a card name: ").title()
+# Created a seperate decklist class, which deals with finding, adding and removing cards from the opened decklist
+from decklist import Decklist
 
-try:
-    # Currently provided the name, colour identity, mana cost and sets of card entered
-    card = Card.where(name=query).all()
-    for i in card:
-        if i.name == query:
-            print(i.name, i.colors, i.cmc, i.set)
+def find_card():
+    query = input("Please input a card name: ").title()
 
-    print(f"The converted mana cost of {card} is {card.cmc}")
+    try:
+        # Currently provided the name, colour identity, mana cost and sets of card entered
+        # Eventually we want to only return 1 card, not worried out sets for now. This is currently here so I can see all the results I need
+        card = Card.where(name=query).all()
 
-except Exception as error:
-    print("Error: ", error)
+        # .all() retuns all cards that contain the entered word, whereas im looking for specific matches, which is the loop below
+        for i in card:
+            if i.name == query:
+                print(i.name, i.colors, i.cmc, i.set)
+                print(i)
+
+        print(f"The converted mana cost of {card[0].name} is {card[0].cmc}")
+        return(card[0])
+
+    # Index error means that the card wasnt found
+    except IndexError:
+        print("Card not found, please try again.")
+        find_card()
+    except Exception as error:
+        print("Error: ", error)
+    
+
+def open_decklist():
+    # If the input decklist exists in saved_decklists folder, open the file, then create a list using the multiverse_ids, which will be the decklist for class creation
+    # Also passes the decklist name to the class
+    pass
+
+def new_decklist():
+    # Creates a new blank list, and creates a class with no values yet
+    # Decklist_name will also be a blank screen, as this can be chosen when saving
+    pass
+
+def show_saved_decklists():
+    pass
+
+def delete_decklist():
+    pass
+
+def main():
+    find_card()
+    # Creating a basic menu, that allows the user to open previously saved decklists, create a new one, and add or remove cards from the decklists
+    menu_choice = input('''
+Please choose from the following options: 
+    1. Open Decklist
+    2. Create New Decklist
+    3. Show Saved Decklists
+    4. Delete Decklist
+''')
+
+    if menu_choice == 1:
+        open_decklist()
+    elif menu_choice == 2:
+        new_decklist()
+    elif menu_choice == 3:
+        show_saved_decklists
+    elif menu_choice == 4:
+        delete_decklist()
+    else:
+        print("Invalid choice, please choose again")
+        
+
+
+    decklist = open_decklist()
+
+
+main()
